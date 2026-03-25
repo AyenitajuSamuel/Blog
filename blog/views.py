d54b3d1c88841a4a44e9f4c608c599631358552e
Samuel <samuelayenitaju@gmail.com>
2026-03-25 21:08:24 +0100

    feat: create post models and setup admin, urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return HttpResponse(f"There are {posts.count()} posts in this app.")
