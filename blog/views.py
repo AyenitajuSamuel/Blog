f2a9303d5f88c0200443c8b491d28854fd6a5798
Samuel <samuelayenitaju@gmail.com>
2026-03-30 20:32:47 +0100

    feat: add dynamic post urls to blog views.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.all()
-    return HttpResponse(f"There are {posts.count()} posts in this app.")
+    return render(request, 'blog/post_list.html', {"posts":posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_details.html', {"post": post})
