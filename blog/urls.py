28d59323a4ccc13aded5c1a2bc006799bc0b0d0f
Samuel <samuelayenitaju@gmail.com>
2026-03-31 21:36:50 +0100

    feat: implement create operation to posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, post_create
 
 urlpatterns = [
     path("", post_list, name='post_list'),
-    path("posts/<int:pk>/", post_detail, name='post_detail')
+    path("posts/<int:pk>/", post_detail, name='post_detail'),
+    path("posts/create/", post_create, name="post_create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 
 def post_list(request):
@@ -10,3 +11,14 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_details.html', {"post": post})
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_create.html', {"form": form})
