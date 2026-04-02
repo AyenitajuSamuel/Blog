db01ec9c8d8083988162d3de9fb747a8562d96c2
Samuel <samuelayenitaju@gmail.com>
2026-04-02 22:15:43 +0100

    feat: implement edit operations for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list, post_detail, post_create
+from .views import post_list, post_detail, post_create, post_edit
 
 urlpatterns = [
     path("", post_list, name='post_list'),
-    path("posts/<int:pk>/", post_detail, name='post_detail'),
-    path("posts/create/", post_create, name="post_create"),
+    path("post/<int:pk>/", post_detail, name='post_detail'),
+    path("post/create/", post_create, name="post_create"),
+    path("post/<int:pk>/edit/", post_edit, name="post_edit"),
 ]
