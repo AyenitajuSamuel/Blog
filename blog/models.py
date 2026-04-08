053a076f042e50401ffb0b2778cb968e961fe932
Samuel <samuelayenitaju@gmail.com>
2026-04-08 07:55:19 +0100

    feat: prevent unauthorized users from accessing certain views.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +8,7 @@ class Post(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -11,17 +12,21 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post_detail.html', {"post": post})
 
+@login_required
 def post_create(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
             return redirect('post_list')
     else:
         form = PostForm()
 
     return render(request, 'blog/post_create.html', {"form": form})
 
+@login_required
 def post_edit(request, slug):
     post = get_object_or_404(Post, slug=slug)
     if request.method == "POST":
@@ -34,6 +39,7 @@ def post_edit(request, slug):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_delete(request, slug):
     post = get_object_or_404(Post, slug=slug)
     if request.method == "POST":
